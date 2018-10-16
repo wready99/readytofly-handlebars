23467ecca3040fedf0533f5dc08b38a24444b25e
wready99 <w_ready@yahoo.com>
2018-10-15 19:52:49 -0700

    first commit

---
+++ static/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_Zfc4sAoxQ',
+        userPoolClientId: '6s7162jdp2gtimm64njo4gvhkr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
